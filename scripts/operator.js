cc9cad642465e20e770cd8fd67d660b72986193c
Anusha-website <anushabcse2000@gmail.com>
2022-11-30 20:43:15 +0530

    1st.js

---
+++ scripts/operator.js
@@ -0,0 +1,34 @@
+//Arthimatic operators
+let a=5;
+let b=12;
+Addition=a+b
+substartion=a-b
+mutiplication:a*b
+division:a/b
+module:a%b
+console.log(Addition,substraction,multiplication,division,module)
+
+//Assignment operator
+ let x=7;
+ let y=9;
+ console.log(x+=y,x-=y,x*=y,x/=y,x%=y,x**=y)
+
+//logical operator
+//local OR operator
+let VAl=12;
+let VAL=20;
+console.log(VAl>=0&&VAL>=0)
+console.log(VAl>=20||VAL<=12)
+console.log(VAl!=VAL)
+//compaison operator
+let p=18;
+let q=21;
+console.log(p>q,p<q,p<=q,q>=p,p==q,p===q)
+
+//increment and decrement operator
+let Num=6;
+Num++;
+++Num;
+Num--;
+--Num;
+console.log(Num)
