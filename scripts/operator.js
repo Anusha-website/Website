3fd3e06e720820631abb19c5d36b51b961bf7ba3
Anusha-website <anushabcse2000@gmail.com>
2022-12-01 15:44:24 +0530

    day1

--- scripts/operator.js
+++ scripts/operator.js
@@ -1,34 +1,41 @@
 //Arthimatic operators
 let a=5;
 let b=12;
-Addition=a+b
-substartion=a-b
-mutiplication:a*b
-division:a/b
-module:a%b
-console.log(Addition,substraction,multiplication,division,module)
+let addition=a+b
+let substartion=a-b
+let mutiplication=a*b
+let division=a/b
+let modulos=a%b
+console.log(addition,substartion,mutiplication,division,modulos)
 
 //Assignment operator
  let x=7;
  let y=9;
  console.log(x+=y,x-=y,x*=y,x/=y,x%=y,x**=y)
-
-//logical operator
-//local OR operator
-let VAl=12;
-let VAL=20;
-console.log(VAl>=0&&VAL>=0)
-console.log(VAl>=20||VAL<=12)
-console.log(VAl!=VAL)
-//compaison operator
-let p=18;
-let q=21;
-console.log(p>q,p<q,p<=q,q>=p,p==q,p===q)
-
-//increment and decrement operator
-let Num=6;
-Num++;
-++Num;
-Num--;
---Num;
-console.log(Num)
+ //logical operators
+//logical  And operator
+let age = 20;
+if(age>13 && age<=19){
+    console.log(`you are a "teenager".`)
+} else if(age>=19 && age<27){
+    console.log(`"you are an "adult"`)
+}else{console.log("you are not a teenager or adult")}
+// logical OR operator
+let num=8;
+let num1=10;
+console.log(num>num1 || num<num1)
+//logical not operator
+let three=3;
+let seven=7;
+console.log(!(three>seven && three<seven))
+//Ternary
+let result = (8<5) ?"True":"False";
+console.log(result)
+// incriment and decriment
+let N = 3;
+console.log(N++)//3
+console.log(++N)//5
+console.log(N--)//5
+console.log(++N)//5
+console.log(--N)//4
+console.log(N)//4
