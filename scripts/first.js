7e728a220c91d1de1abee25c111a63fcc24f6cb7
Anusha-website <anushabcse2000@gmail.com>
2022-12-04 13:27:38 +0530

    day

--- scripts/first.js
+++ scripts/first.js
@@ -1,41 +1,89 @@
 //Arthimatic operators
-let a=5;
-let b=12;
-let addition=a+b
-let substartion=a-b
-let mutiplication=a*b
-let division=a/b
-let modulos=a%b
-console.log(addition,substartion,mutiplication,division,modulos)
+//let a=5;
+//let b=12;
+//let addition=a+b
+//let substartion=a-b
+//let mutiplication=a*b
+//let division=a/b
+//let modulos=a%b
+//console.log(addition,substartion,mutiplication,division,modulos)
 
 //Assignment operator
- let x=7;
- let y=9;
- console.log(x+=y,x-=y,x*=y,x/=y,x%=y,x**=y)
+ //let x=7;
+ //let y=9;
+ //console.log(x+=y,x-=y,x*=y,x/=y,x%=y,x**=y)
  //logical operators
 //logical  And operator
-let age = 20;
-if(age>13 && age<=19){
-    console.log(`you are a "teenager".`)
-} else if(age>=19 && age<27){
-    console.log(`"you are an "adult"`)
-}else{console.log("you are not a teenager or adult")}
+//let age = 20;
+//if(age>13 && age<=19){
+   // console.log(`you are a "teenager".`)
+//} else if(age>=19 && age<27){
+   // console.log(`"you are an "adult"`)
+//}else{console.log("you are not a teenager or adult")}
 // logical OR operator
-let num=8;
-let num1=10;
-console.log(num>num1 || num<num1)
+//let num=8;
+//let num1=10;
+//console.log(num>num1 || num<num1)
 //logical not operator
-let three=3;
-let seven=7;
-console.log(!(three>seven && three<seven))
+//let three=3;
+//let seven=7;
+//console.log(!(three>seven && three<seven))
 //Ternary
-let result = (8<5) ?"True":"False";
-console.log(result)
+//let result = (8<5) ?"True":"False";
+//console.log(result)
 // incriment and decriment
-let N = 5;
-console.log(N++)//5
-console.log(++N)//7
-console.log(N--)//7
-console.log(++N)//7
-console.log(--N)//6
-console.log(N)//6
+//let N = 5;
+//console.log(N++)//5
+//console.log(++N)//7
+//console.log(N--)//7
+//console.log(++N)//7
+//console.log(--N)//6
+//console.log(N)//6
+//switch case
+//let x=7;
+    //switch (days) {
+        //case  day0:
+          //console.log("monday");
+        //break;
+        //case day1:
+            //console.log("tuesday");
+        //break;   
+        //case day2:
+            //console.log("wednesday");    
+        //break;
+        //case day3:
+            //console.log("thursday");
+           // break;
+            //case day4:
+                //console.log("friday");
+                //break;
+            //case day6:
+                //console.log("saturday");
+           // break;
+           // case day7:
+                //console.log("sunday");       
+            //default:
+               // console.log("something write to get wrong");
+   // }
+   //for loop
+   //let i=10;
+   //for(let i=0;i<=10;i++){
+     // console.log(i);
+   //}
+//while loop
+//let i= 0;
+
+//while (i< 10) {
+  //i++;
+//}
+
+//console.log(i);
+
+//do-while loop
+let i=0;
+do {
+    
+} while (i<=10){
+  i--;
+}
+  console.log(i); 
