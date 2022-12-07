4f7356fcc6e3066c50db6c00d8ee941dc93e4c02
Anusha-website <anushabcse2000@gmail.com>
2022-12-07 08:56:38 +0530

    work

--- scripts/anusha.js
+++ scripts/anusha.js
@@ -1,6 +1,21 @@
-let x;
-x=prompt("what is youe name");
-alert("key "+x+" have a good day");
+//let x;
+//x=prompt("what is youe name");
+//alert("key "+x+" have a good day");
 
-let person=prompt("Enter your name");
-alert("Hi "+person+" Welcome!");
+//let person=prompt("Enter your name");
+//alert("Hi "+person+" Welcome!");
+
+letnum1= parseInt(prompt("please enter your num1"));
+letnum2=parseInt(prompt("please enter your num2"));
+letsum=(num1+num2);
+let sub = num1-num2;
+let mul = num1*num2;
+let division = num1/num2;
+let mod = num1&num2;
+alert(`Sum of ${num1} and ${num2} is ${sum}`);
+alert( `substract of ${num1} and ${num2} is ${sub}`);
+alert( `multiplication of ${num1} and ${num2} is ${mul}`);
+alert(`division of ${num1} and ${num2} is ${division}` );
+alert(`modulos of ${num1} and ${num2} is ${mod}` );
+
+
