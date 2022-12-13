c7b7666df1cc955b9bd740e7fc59be88080cef1f
Anusha-website <anushabcse2000@gmail.com>
2022-12-13 08:25:49 +0530

    work

--- scripts/task.js
+++ scripts/task.js
@@ -8,16 +8,50 @@ const sixthBox=document.getElementById("box6");
 
 document.getElementById("box1").innerHTML+=`this is box1`;
 firstBox.style.backgroundColor = "blue";
+firstBox.style.height="350px";
+firstBox.style.width="350px";
+firstBox.style.float="left";
+firstBox.style.marginLeft="10px";
+firstBox.style.marginTop="10px";
 document.getElementById("box2").innerHTML+=`this is box2`;
 secoundBox.style.backgroundColor = "green";
+secoundBox.style.height="350px";
+secoundBox.style.width="350px";
+secoundBox.style.float="left";
+secoundBox.style.marginLeft="10px";
+secoundBox.style.marginTop="10px";
 document.getElementById("box3").innerHTML+=`this is box3`;
 thirdBox.style.backgroundColor = "red";
+thirdBox.style.height="350px";
+thirdBox.style.width="350px";
+thirdBox.style.float="left";
+thirdBox.style.marginLeft="10px";
+thirdBox.style.marginTop="10px";
+
 document.getElementById("box4").innerHTML+=`this is box4`;
 fourthBox.style.backgroundColor = "purple";
+fourthBox.style.height="350px";
+fourthBox.style.width="350px";
+fourthBox.style.float="left";
+fourthBox.style.marginLeft="10px";
+fourthBox.style.marginTop="10px";
+
 document.getElementById("box5").innerHTML+=`this is box5`;
 fifthBox.style.backgroundColor = "orange";
+fifthBox.style.height="350px";
+fifthBox.style.width="350px";
+fifthBox.style.float="left";
+fifthBox.style.marginLeft="10px";
+fifthBox.style.marginTop="10px";
+
 document.getElementById("box6").innerHTML+=`this is box6`;
 sixthBox.style.backgroundColor = "skyblue";
+sixthBox.style.height="350px";
+sixthBox.style.width="350px";
+sixthBox.style.float="left";
+sixthBox.style.marginLeft="10px";
+sixthBox.style.marginTop="10px";
+
 
 
 
