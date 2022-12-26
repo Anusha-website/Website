05e0da718cf1102a397797ac42652e5717ce649e
Anusha-website <anushabcse2000@gmail.com>
2022-12-26 17:00:49 +0530

    work

--- scripts/preview.js
+++ scripts/preview.js
@@ -31,5 +31,34 @@ createDeveloper({
     favfruit:"orange",
     company:"Amazon"
 },getDevelopers);
-    
+ 
 
+//promises
+function createDeveloper(dev){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            if(dev &&Object.keys(dev).length===0&&Object.getPrototypeOf(dev)===Object.prototype){
+                reject("Empty Dev Recieved.Please Fix");
+            }else{
+                resolve(developers.push(dev));
+            }
+        },3000);
+    })
+}
+
+createDeveloper({
+    name:"Ramesh",
+    favfruit:"orange",
+    company:"Amazon"
+}).then(getDevelopers).catch(err => console.err(err)).finally("We're done");
+
+//Async Await
+async function initProgram(){
+    await createDeveloper({
+    name:"Ramesh",
+    favfruit:"Orange",
+    company:"Amazon"
+    });
+    getDevelopers();
+}    
+initProgram();
